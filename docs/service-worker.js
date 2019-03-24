4948dc6fd512af163f4c3d02bea7e0cf49136857
robertu <robertu@robertu.net>
2019-03-24 12:07:37 +0100

    new docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83942a669c07db1ee0adc3f52270a951.js"
+  "/mosaicrwm-sample-app1/precache-manifest.2b9f0016b18b96a7ab0eea37a18652cf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mosaicrwm-sample-app1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
